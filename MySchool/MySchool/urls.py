3f2ffc58b1c42def2293f53cab239ccfe1438b2c
Nijo Ninan <nijoninan23@gmail.com>
2021-06-16 12:28:06 +0530

    Added favicon, few minor updates on templates and Redirect added to empty path

--- MySchool/MySchool/urls.py
+++ MySchool/MySchool/urls.py
@@ -22,8 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',RedirectView.as_view(url='home/')),
-    # path('home/', include('home.urls')),
+    path('',RedirectView.as_view(url='accounts/profile/')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('teacher/', include('teacher.urls')),
